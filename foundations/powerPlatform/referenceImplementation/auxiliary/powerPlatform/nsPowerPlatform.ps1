aee15b8e02b276749d383ca461e412ad37f04e9c
HemantKumar10 <hemantkumargrs@gmail.com>
2024-02-12 20:08:11 +0530

    Update nsPowerPlatform.ps1

--- foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
+++ foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
@@ -28,9 +28,9 @@ param (
     [Parameter(Mandatory = $false)][string][AllowEmptyString()][AllowNull()]$PPCitizenAlm,    
     [Parameter(Mandatory = $false)][string][AllowEmptyString()][AllowNull()]$PPCitizenCurrency,
     [Parameter(Mandatory = $false)][string][AllowEmptyString()][AllowNull()]$PPCitizenLanguage,     
-    [Parameter(Mandatory = $false)][string][AllowEmptyString()][AllowNull()]$ppD365SalesApp,
-    [Parameter(Mandatory = $false)][string][AllowEmptyString()][AllowNull()]$ppD365CustomerServiceApp,
-    [Parameter(Mandatory = $false)][string][AllowEmptyString()][AllowNull()]$ppD365FieldServiceApp        
+    [Parameter(Mandatory = $false)][bool][AllowEmptyString()][AllowNull()]$ppD365SalesApp,
+    [Parameter(Mandatory = $false)][bool][AllowEmptyString()][AllowNull()]$ppD365CustomerServiceApp,
+    [Parameter(Mandatory = $false)][bool][AllowEmptyString()][AllowNull()]$ppD365FieldServiceApp        
 )
 
 $DeploymentScriptOutputs = @{}
