1533d05a9ce9560b3b73b9d75c1b18b5e66d4cff
HemantKumar10 <hemantkumargrs@gmail.com>
2024-02-12 12:05:22 +0530

    Update nsPowerPlatform.ps1

--- foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
+++ foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
@@ -457,17 +457,15 @@ if ($PPCitizen -in "yes", "half" -and $PPCitizenCount -ge 1 -or $PPCitizen -eq '
                 SecurityGroupId    = $environment.envRbac  
                 Templates          = 'D365_Sales'                             
             }   
-            $null = New-PowerOpsEnvironment @envCreationHt 
-            Write-Output "Created citizen environment $($environment.envName) in $($environment.envRegion)"
-            
-            Write-Output "D365 for Sales: $ppD365SalesApp"
-            Write-Output "D365 for Customer Service: $ppD365CustomerServiceApp"
-            Write-Output "D365 for Field Service: $ppD365FieldServiceApp"        
-            
-            if (-not [string]::IsNullOrEmpty($environment.envRbac) -and $environment.envDataverse -eq $false) {
-                Write-Output "Assigning RBAC for principalId $($environment.envRbac) in citizen environment $($environment.envName)"
-                $null = New-PowerOpsRoleAssignment -PrincipalId $environment.envRbac -RoleDefinition EnvironmentAdmin -EnvironmentName $environment.envName
-            }      
+
+            $envCreation = @{
+                EnvironmentPrefix   = 'PowerPlatform'
+                Location            = 'unitedkingdom'
+                                          
+            }          
+            createEnvrionment @envCreation
+            Write-Output "Created citizen environment"            
+               
            
         }
         catch {
@@ -662,6 +660,119 @@ function New-AdminPowerAppEnvironments
             }
         }
     }
+}
+
+
+function createEnvrionment {
+    [CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $EnvironmentPrefix,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('unitedstates', 'europe', 'asia', 'australia', 'india', 'japan', 'canada', 'unitedkingdom', 'unitedstatesfirstrelease', 'southamerica', 'france', 'switzerland', 'germany', 'unitedarabemirates')]
+    [String]
+    $Location
+)
+
+$Environments = @()
+$Environments += "$($EnvironmentPrefix)-prod"
+$Environments += "$($EnvironmentPrefix)-dev"
+$Environments += "$($EnvironmentPrefix)-test"
+
+# Get token to authenticate to Power Platform
+
+$Token = (Get-AzAccessToken).Token
+
+# Power Platform API base Uri
+$BaseUri = "https://api.bap.microsoft.com"
+
+# Power Plaform HTTP Get Environment Uri
+$GetEnvironment = '/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments?$expand=permissions&api-version=2016-11-01'
+
+# Power Platform HTTP Post Environment Uri
+$PostEnvironment = '/providers/Microsoft.BusinessAppPlatform/environments?api-version=2019-05-01&ud=/providers/Microsoft.BusinessAppPlatform/scopes/admin/environments'
+
+# Power Platform HTTP Get DLP Policy Uri // Coming soon
+# $GetPolicies = "https://api.bap.microsoft.com/providers/Microsoft.BusinessAppPlatform/scopes/admin/apiPolicies?api-version=2016-11-01"
+
+# Declare Rest headers
+$Headers = @{
+    "Content-Type"  = "application/json"
+    "Authorization" = "Bearer $($Token)"
+}
+
+foreach ($Env in $Environments) {
+    Write-Output "Creating Environment: $($Env)"
+
+    # Form the request body to create new Environments in Power Platform
+    # Declaring the HTTP Post request
+
+    $PostBody = @{
+        "properties" = @{
+            "linkedEnvironmentMetadata" = @{
+                "baseLanguage" = ''
+                "domainName"   = "$($Env)"
+                "templates"    = @("D365_DeveloperEdition")
+            }
+            "databaseType"   = "CommonDataService"
+            "displayName"    = "$($Env)"
+            "environmentSku" = "Production"
+        }
+        "location"   = "$($Location)"
+    }
+
+    $PostParameters = @{
+        "Uri"         = "$($baseUri)$($postEnvironment)"
+        "Method"      = "Post"
+        "Headers"     = $headers
+        "Body"        = $postBody | ConvertTo-json -Depth 100
+        "ContentType" = "application/json"
+    }
+
+    Write-Output "Invoking the request to create Environment: $($Env)"
+
+    try {
+        $response = Invoke-RestMethod @PostParameters
+        Write-Output "Environment $($Env) is being created..."
+    }
+    catch {
+        Write-Error "Creation of Environment $($Env) failed`r`n$_"
+        throw "REST API call failed drastically"
+    }
+
+    # Get newly created environments
+
+    $GetParameters = @{
+        "Uri"         = "$($BaseUri)$($GetEnvironment)"
+        "Method"      = "Get"
+        "Headers"     = $headers
+        "ContentType" = "application/json"
+    }
+
+    Write-Output "Checking environment status for $($Env)"
+    Start-Sleep -Seconds 30
+    try {
+        $response = Invoke-RestMethod @GetParameters
+    }
+    catch {
+        Write-Output "Retrieving the environment failed.`r`n$_"
+        throw "Ouch...."
+    }
+    $response.value.properties | Where-Object { $_.displayName -eq $($Env) } | Sort-Object -Property createdTime -Descending -Top 1 | Foreach-Object -Process {
+        [PSCustomObject]@{
+            Name              = $_.displayName
+            environmentType   = $_.environmentType
+            provisioningState = $_.provisioningState
+            azureRegionHint   = $_.azureRegionHint
+            createdTime       = $_.createdTime
+            resourceId        = $_.linkedEnvironmentMetadata.resourceId
+        }
+    }
+}
 }
 
 $DeploymentScriptOutputs['Deployment'] = 'Successful'
