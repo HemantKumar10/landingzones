1e46fdf33b95b0b8d498fbc244b76828086218cc
HemantKumar10 <hemantkumargrs@gmail.com>
2024-03-05 11:05:57 +0530

    Update nsPowerPlatform.ps1

--- foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
+++ foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
@@ -101,11 +101,9 @@ function New-EnvironmentCreationObject {
 function New-CreateSecurityGroup {
     param (      
         [Parameter(Mandatory = $true)][string]$EnvironmentType
-    )
+    )   
 
-   
-
-        $devSecurityGroup = @{
+            $devSecurityGroup = @{
             description="Security Group used for Power Platform - Development environment"
             displayName="entra_powerplatform_development"
             mailEnabled=$false
@@ -113,7 +111,7 @@ function New-CreateSecurityGroup {
             mailNickname="PowerPlatformDevelopmentGroup"
            }
         
-          $testSecurityGroup = @{
+            $testSecurityGroup = @{
             description="Security Group used for Power Platform - Test environment"
              displayName="entra_powerplatform_test"
             mailEnabled=$false
@@ -140,23 +138,11 @@ function New-CreateSecurityGroup {
             $Value =''
             # Code Begins
             # Get token to authenticate to Power Platform                       
-            #$Token = (Get-AzAccessToken -ResourceUrl " https://graph.microsoft.com/.default").Token            
+            $Token = (Get-AzAccessToken -ResourceUrl " https://graph.microsoft.com/.default").Token            
             
             #Write-Output "Bearer $($tokeny)" #> 
             #$Token = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com/v1.0/groups").Token   
 
-            $Token = "eyJ0eXAiOiJKV1QiLCJub25jZSI6InFDLTFlMl9xUHYwb0x0S1hFZFJyZnd1TG56cTV0VklhSVRVVUt6TS1NLWsiLCJhbGciOiJSUzI1NiIsIng1dCI6IlhSdmtvOFA3QTNVYVdTblU3Yk05blQwTWpoQSIsImtpZCI6IlhSdmtvOFA3QTNVYVdTblU3Yk05blQwTWpoQSJ9.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC84MjBmZjI0Mi1hNzU0LTRlN2EtOWJlOS1kZTdiNTM1MDI1MWYvIiwiaWF0IjoxNzA4OTUxNTk0LCJuYmYiOjE3MDg5NTE1OTQsImV4cCI6MTcwODk1NTY4MiwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhXQUFBQUkzRko3dURaN2lLTGxuUi9YTWlLRFF6VVdFL1lYNWFwSlp3dTEzNStkSUJpbXVkR29tSTJJUm1KT0xzSEE1bmpyTXpSbFJ2VzRGelJ1c0pKSVJYbkVib0F6T0NzeGpTMlZHeXF4SU9JU1lFPSIsImFtciI6WyJwd2QiLCJtZmEiXSwiYXBwX2Rpc3BsYXluYW1lIjoiUG9zdG1hbiIsImFwcGlkIjoiNjZkYzllZTktZTMyZi00ZWNlLTliMTktNjA4YjNhMGY0YjZkIiwiYXBwaWRhY3IiOiIxIiwiaWR0eXAiOiJ1c2VyIiwiaXBhZGRyIjoiODIuNS4xODEuMzUiLCJuYW1lIjoiQm9nZGFuIENpb2JhbnUiLCJvaWQiOiJhOTI2N2JmMS03ZDlhLTRmYjgtOTM5ZC01NWM2M2JiMmU5M2MiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDMxRjE2MEE1QiIsInJoIjoiMC5BUXdBUXZJUGdsU25lazZiNmQ1N1UxQWxId01BQUFBQUFBQUF3QUFBQUFBQUFBQU1BTEEuIiwic2NwIjoiRGlyZWN0b3J5LkFjY2Vzc0FzVXNlci5BbGwgRGlyZWN0b3J5LlJlYWQuQWxsIERpcmVjdG9yeS5SZWFkV3JpdGUuQWxsIERpcmVjdG9yeS5Xcml0ZS5SZXN0cmljdGVkIEdyb3VwLlJlYWQuQWxsIEdyb3VwLlJlYWRXcml0ZS5BbGwgTWFpbC5SZWFkIE1haWwuUmVhZC5TaGFyZWQgTWFpbC5SZWFkQmFzaWMgTWFpbC5SZWFkQmFzaWMuU2hhcmVkIE1haWwuUmVhZFdyaXRlIE1haWwuUmVhZFdyaXRlLlNoYXJlZCBNYWlsLlNlbmQgTWFpbC5TZW5kLlNoYXJlZCBVc2VyLkVuYWJsZURpc2FibGVBY2NvdW50LkFsbCBVc2VyLkV4cG9ydC5BbGwgVXNlci5JbnZpdGUuQWxsIFVzZXIuTWFuYWdlSWRlbnRpdGllcy5BbGwgVXNlci5SZWFkIFVzZXIuUmVhZC5BbGwgVXNlci5SZWFkQmFzaWMuQWxsIFVzZXIuUmVhZFdyaXRlIFVzZXIuUmVhZFdyaXRlLkFsbCBwcm9maWxlIG9wZW5pZCBlbWFpbCIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6IjRkMW1qcEhlUkp1cXYxWVRqUk1sekdwQmdQWjRRbTBIS214bXB4YWFPdTQiLCJ0ZW5hbnRfcmVnaW9uX3Njb3BlIjoiRVUiLCJ0aWQiOiI4MjBmZjI0Mi1hNzU0LTRlN2EtOWJlOS1kZTdiNTM1MDI1MWYiLCJ1bmlxdWVfbmFtZSI6ImJvZ2Rhbi5jaW9iYW51QGFuc2NvZW91dGxvb2sub25taWNyb3NvZnQuY29tIiwidXBuIjoiYm9nZGFuLmNpb2JhbnVAYW5zY29lb3V0bG9vay5vbm1pY3Jvc29mdC5jb20iLCJ1dGkiOiJGMzBHV0k2QUhVLXI2U2FjZ3o2UkFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyI2MmU5MDM5NC02OWY1LTQyMzctOTE5MC0wMTIxNzcxNDVlMTAiLCJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX3N0Ijp7InN1YiI6InRlRk05X25YcXBZekprTHYxUThNMmtKWGM1cmtJZkVJQnQ2TURMelhSMGcifSwieG1zX3RjZHQiOjE2MjY0MjQzMzF9.WG9kGGB7Wb18eMEZNZdmGMnwbMg7-CjkpI6dmM6K-yh01Zt8YiVSRvqv_dRDddVTCjI1398pMfa-d542NCWGPlCvAamUryj07oEBH1qsHPhwHyxN5YUf7JsGCaYhZDhI2IH98THj1-tHdPC_Z81JAfCqzeb4cHMIYcL_foCF-PUNAYW1Fja1f5i-E0mZ4HzMCMp8-SCsBJm0e0IKApA_JQzNpSQJSL9-dC6UtO_jCntCh3tAY0BLurGULAo7soj3MkqamkC4IcrFwD7OSJDxstVTjMOfevyqulNAIpxHHXvw2WOlIi50xAbtPetMOMsuZBy-9KxoJ-UqQ_JJchxSjQ"
-        
-            try {
-                $TokenGraph = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com/").Token
-                Write-Output "Graph TOken $($TokenGraph)"  
-            }
-            catch {
-                Write-Error "Graph TOken $($TokenGraph)"  
-            }
-            
-            $Token = $TokenGraph
-        
             # Power Platform HTTP Post Group Uri
             $PostGroups = 'https://graph.microsoft.com/v1.0/groups'
             
@@ -208,8 +194,6 @@ function New-InstallPackaggeToEnvironment {
     ) 
         # Code Begins
         # Get token to authenticate to Power Platform
-        $TokenGraph = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com/").Token
-        
         $Token = (Get-AzAccessToken -ResourceUrl "https://api.powerplatform.com/").Token
         # Power Platform HTTP Post Environment Uri
         $PostEnvironment = "https://api.powerplatform.com/appmanagement/environments/$($EnvironmentId)/applicationPackages/$($PackageName)/install?api-version=2022-03-01-preview"           
@@ -457,7 +441,7 @@ if ($PPCitizen -in "yes")
             # Get token to authenticate to Power Platform
             $Token = (Get-AzAccessToken).Token    
             
-       
+            Write-Output "TOken- $Token"  
 
             
             # Power Platform API base Uri
