1cd07db06696891ea78a82873d44820c31d2cd82
HemantKumar10 <hemantkumargrs@gmail.com>
2024-09-20 19:39:03 +0530

    Update nsPowerPlatform.ps1

--- foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
+++ foundations/powerPlatform/referenceImplementation/auxiliary/powerPlatform/nsPowerPlatform.ps1
@@ -1322,9 +1322,7 @@ if ($PPCitizen -in "yes") {
             
            
         
-            try { 
-                ElevateUsers -EnvironmentId '7c05bbcf-ffa9-eff6-84ee-a8563f462aab'
-
+            try {  
                 $response = Invoke-RestMethod @PostParameters   
                 Write-Output "Create Environment: $($envCreationHt.Name) Completed" 
                 #Code to apply Admin DLP Policy for Admin Env#
